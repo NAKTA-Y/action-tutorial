bcbd824bbb97819f63d6f0ac959c985351c2eab7
nakta-y <nakta.y.biz@gmail.com>
2024-03-04 10:56:43 +0900

    add test

--- src/test/java/org/example/springactiondemo/AddTest.java
+++ src/test/java/org/example/springactiondemo/AddTest.java
@@ -1,10 +1,12 @@
 package org.example.springactiondemo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AddTest {
 
     @Test
     void addTest() {
+        Assertions.assertTrue(true);
     }
 }
